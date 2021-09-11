28e179b03cb160a0d8aa79eadcc786ef5b9d24df
USER <USER@DESKTOP-6N8H20N>
2021-09-10 19:54:43 -0500

    T1

---
+++ ROSMERY_T1_V2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ROSMERY_T1_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
