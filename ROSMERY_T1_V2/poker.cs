c48137f3b8cc5edba44fda1a04f9332f6023343a
USER <USER@DESKTOP-6N8H20N>
2021-09-10 21:01:14 -0500

    T1 1

--- ROSMERY_T1_V2/poker.cs
+++ ROSMERY_T1_V2/poker.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace ROSMERY_T1_V2
 {
-    class poker
+    public class poker
     {
+        public int Id { get; set; }
+        public int Numero { get; set; }
+        public string carta { get; set; }
+        public int IdUsuario { get; set; }
+
+        public poker(int id, int numero, string carta, int Id_usuario)
+        {
+            Id = id;
+            Numero = numero;
+            this.carta = carta;
+            IdUsuario = IdUsuario;
+        }
     }
 }
